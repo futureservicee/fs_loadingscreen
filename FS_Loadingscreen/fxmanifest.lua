c8d1dc8e280a46c519be591b525b6e047eaf4847
Future Service <159530975+futureservicee@users.noreply.github.com>
2024-02-15 13:10:12 +0100

    Add files via upload

---
+++ FS_Loadingscreen/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Discord.gg/futureservice'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+
+
+loadscreen_cursor 'yes'
+
+files {
+    'html/index.html',
+    'html/assets/css/styles.css',
+    'html/assets/css/styles.css.map',
+    'html/assets/scss/styles.scss',
+    'html/assets/images/*.png',
+    'html/assets/images/*.svg',
+    'html/assets/sounds/music.mp3'
+ }
